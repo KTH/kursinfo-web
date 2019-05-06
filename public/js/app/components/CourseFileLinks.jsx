e3f14f87509f035797ae9d3efe17c686e2251d18
Jeanette Skoog <jeanetteskoog@n129-p226.eduroam.kth.se>
2019-05-06 13:59:41 +0200

    cleaning up

--- public/js/app/components/CourseFileLinks.jsx
+++ public/js/app/components/CourseFileLinks.jsx
@@ -1,65 +1,37 @@
 import { Component } from 'inferno'
 import Row from 'inferno-bootstrap/dist/Row'
 import Col from 'inferno-bootstrap/dist/Col'
-import i18n from "../../../../i18n"
-import { EMPTY } from "../util/constants"
+import i18n from '../../../../i18n'
+import { EMPTY } from '../util/constants'
 
 class CourseFileLinks extends Component {
- constructor (props) {
-    super(props)
-    //this.openSyllabus=this.openSyllabus.bind(this)
-  }
-
-  /*openSyllabus(event){
-    event.preventDefault()
-    const language = this.props.language === 0 ? "en" : "sv" 
-    window.open(`/student/kurser/kurs/kursplan/${this.props.courseCode}_${event.target.id}.pdf?lang=${language}`)
-  }*/
-
   render () {
-    //console.log("this.props.courseRound", this.props)
     const translate = i18n.messages[this.props.language].courseRoundInformation
     const round = this.props.courseRound
     return (
-      <Row id="courseLinks">
-        <Col sm="12" xs="12">
-        {/* ---LINK TO SYLLABUS IF COURSE HAS ONE-- */}
-          {this.props.syllabusValidFrom.length > 0 ?
-          <span>
-            <i class="fas fa-file-pdf"></i> 
-            <a href="javascript" onClick={this.openSyllabus} id={this.props.syllabusValidFrom[0] + this.props.syllabusValidFrom[1]}>
-                {i18n.messages[this.props.language].courseLabels.label_course_syllabus}
+      <Row id='courseLinks'>
+        {/* ---LINK TO ROUND PM/MEMO IF ROUND HAS ONE-- */}
+        <Col sm='12' xs='12'>
+          <i class='fas fa-file-pdf'></i>
+          {i18n.messages[this.props.language].courseLabels.no_memo}
+          {/*
+            <a href="pm-url" id={"pm_"}>
+            {i18n.messages[this.props.language].courseLabels.label_course_pm}
+            </a> ( 20xx-xx-xx )*/}
+        </Col>
+        {/* ---LINK TO ROUND SCHEDULE-- */}
+        <Col sm='12' xs='12'>
+          <i className='icon-schedule'></i>
+          {this.props.scheduleUrl !== EMPTY[this.props.language]
+            ? <a target='_blank' href={this.props.scheduleUrl} >
+              {i18n.messages[this.props.language].courseLabels.label_schedule}
             </a>
-          <span className="small-text" >
-          &nbsp;( {i18n.messages[this.props.language].courseLabels.label_course_syllabus_valid_from }&nbsp; 
-            {i18n.messages[this.props.language].courseInformation.course_short_semester[this.props.syllabusValidFrom[1]]}  {this.props.syllabusValidFrom[0]} )
-          </span>
-          </span>
-          : "" }
-       </Col>
-       {/* ---LINK TO ROUND PM/MEMO ?IF ROUND HAS ONE-- */}
-        <Col sm="12" xs="12">
-          <i class="fas fa-file-pdf"></i>
-          {i18n.messages[this.props.language].courseLabels.no_memo} 
-         {/*<a href="pm-url" id={"pm_"}>
-            {i18n.messages[this.props.language].courseLabels.label_course_pm} 
-          </a> ( 20xx-xx-xx )*/} 
-          </Col>
-         {/* ---LINK TO ROUND SCHEDULE-- */}
-        <Col sm="12" xs="12">
-        <i className="icon-schedule"></i> 
-        {this.props.scheduleUrl !== EMPTY[this.props.language] ?
-          <a target="_blank" href={this.props.scheduleUrl} >
-          {i18n.messages[this.props.language].courseLabels.label_schedule}
-          </a>
-          :
-          <span>{i18n.messages[this.props.language].courseLabels.no_schedule}</span>
+            : <span>{i18n.messages[this.props.language].courseLabels.no_schedule}</span>
         }
-          </Col>
+        </Col>
       </Row>
     )
   }
-} 
-
+}
 
 export default CourseFileLinks
--- public/js/app/components/CourseKeyInformationOneCol.jsx
+++ public/js/app/components/CourseKeyInformationOneCol.jsx
@@ -1,7 +1,6 @@
 import { Component } from 'inferno'
 import Row from 'inferno-bootstrap/dist/Row'
 import Col from 'inferno-bootstrap/dist/Col'
-import Alert from 'inferno-bootstrap/dist/Alert'
 import Button from 'inferno-bootstrap/dist/Button'
 
 
@@ -14,7 +13,6 @@ import InfoModal from './InfoModal.jsx'
 class CourseKeyInformationOneCol extends Component {
   constructor (props) {
     super(props)
-
     this.openApplicationLink = this.openApplicationLink.bind(this)
   }
 
@@ -30,8 +28,8 @@ class CourseKeyInformationOneCol extends Component {
     const course = this.props.courseData
     const roundHeader = translate.round_header
     const selectedRoundHeader = `
-    ${i18n.messages[this.props.language].courseInformation.course_short_semester[round.round_course_term[1]]} 
-    ${round.round_course_term[0]}  
+      ${i18n.messages[this.props.language].courseInformation.course_short_semester[round.round_course_term[1]]} 
+      ${round.round_course_term[0]}  
       ${round.round_short_name !== EMPTY[this.props.language] ? round.round_short_name : ''}     
       ${round.round_type}
     `
@@ -45,25 +43,26 @@ class CourseKeyInformationOneCol extends Component {
           <Col sm='12' id='roundKeyInformation'>
             <div className={` fade-container ${this.props.fade === true ? ' fadeOutIn' : ''}`} key='fadeDiv1'>
 
-            {/* ---COURSE ROUND HEADER--- */}
-            {this.props.courseHasRound && this.props.showRoundData ?
-              <div style='border-bottom:1px solid #fff;'>
-                <h4>{roundHeader} </h4>
-                <p>{selectedRoundHeader}</p>
-              </div>
-              : ''}
+              {/* ---COURSE ROUND HEADER--- */}
+              {this.props.courseHasRound && this.props.showRoundData
+                ? <div style='border-bottom:1px solid #fff;'>
+                  <h4>{roundHeader} </h4>
+                  <p>{selectedRoundHeader}</p>
+                </div>
+                : ''
+              }
 
               {/* ---COURSE ROUND INFORMATION--- */}
-             {this.props.courseHasRound && this.props.showRoundData ?
-                <span>
+              {this.props.courseHasRound && this.props.showRoundData
+                ? <span>
                   <h4>{translate.round_target_group}</h4>
-                  <span dangerouslySetInnerHTML={{ __html:round.round_target_group }}></span>
+                  <span dangerouslySetInnerHTML={{ __html: round.round_target_group }}></span>
 
                   <h4>{translate.round_part_of_programme}</h4>
-                  <span dangerouslySetInnerHTML={{ __html:round.round_part_of_programme }}></span>
+                  <span dangerouslySetInnerHTML={{ __html: round.round_part_of_programme }}></span>
 
                   <h4>{translate.round_periods}</h4>
-                  <span dangerouslySetInnerHTML={{ __html:round.round_periods }}></span>
+                  <span dangerouslySetInnerHTML={{ __html: round.round_periods }}></span>
 
                   <h4>{translate.round_start_date}</h4>
                   <p className='clear-margin-bottom'><i class='fas fa-hourglass-start'></i>{round ? round.round_start_date : EMPTY[this.props.language]}</p>
@@ -72,7 +71,7 @@ class CourseKeyInformationOneCol extends Component {
                   <h4>{translate.round_course_place}</h4>
                   <p>{round ? round.round_course_place : EMPTY[this.props.language]}</p>
                 </span>
-              :
+                :
               //* ---SELECT A ROUND BOX --- *//
                 <span className='text-center2'>
                   <h4>{i18n.messages[this.props.language].courseLabels.header_no_round_selected}</h4>
@@ -83,10 +82,10 @@ class CourseKeyInformationOneCol extends Component {
               {/** *************************************************************************************************************/}
               {/*                                            Round  information                                               */}
               {/** *************************************************************************************************************/}
-              {this.props.courseHasRound && this.props.showRoundData ?
-                <span>
+              {this.props.courseHasRound && this.props.showRoundData
+                ? <span>
                   <h4>{translate.round_tutoring_form}</h4>
-                  <p>{round ? translate.round_tutoring_form_label[round.round_tutoring_form] : EMPTY[this.props.language]}                   {round ? translate.round_tutoring_time_label[round.round_tutoring_time] : EMPTY[this.props.language]}</p>
+                  <p>{round ? translate.round_tutoring_form_label[round.round_tutoring_form] : EMPTY[this.props.language]}                       {round ? translate.round_tutoring_time_label[round.round_tutoring_time] : EMPTY[this.props.language]}</p>
 
                   <h4>{translate.round_tutoring_language}</h4>
                   <p>{round ? round.round_tutoring_language : EMPTY[this.props.language]}</p>
@@ -100,19 +99,14 @@ class CourseKeyInformationOneCol extends Component {
                   <p>{round ? round.round_seats : EMPTY[this.props.language]}</p>
 
                   <h4>{translate.round_time_slots}</h4>
-                  <p dangerouslySetInnerHTML={{ __html:round.round_time_slots }}></p>
-
-
+                  <p dangerouslySetInnerHTML={{ __html: round.round_time_slots }}></p>
                   <CourseFileLinks
-                    index=''
                     language={this.props.language}
                     courseHasRound={this.props.courseHasRound}
-                    syllabusValidFrom=''
                     courseCode={course.course_code}
                     scheduleUrl={round ? round.round_schedule : EMPTY[this.props.language]}
                   />
-
-              </span>
+                </span>
               : ''}
 
             </div>
@@ -123,8 +117,8 @@ class CourseKeyInformationOneCol extends Component {
             {/** *************************************************************************************************************/}
             {/*                                     Round - application information                                         */}
             {/** *************************************************************************************************************/}
-            {this.props.courseHasRound && this.props.showRoundData ?
-              <span>
+            {this.props.courseHasRound && this.props.showRoundData
+              ? <span>
                 <h3 className='right-column-header'>{i18n.messages[this.props.language].courseLabels.header_select_course}</h3>
 
                 <h4>{roundHeader} </h4>
@@ -133,53 +127,50 @@ class CourseKeyInformationOneCol extends Component {
                 <h4>{translate.round_application_code} </h4>
                 <p><b>{round ? round.round_application_code : EMPTY[this.props.language]}</b></p>
                 {round && round.round_application_link !== EMPTY[this.props.language]
-                  ? <Button name={translate.round_application_link} color='primary' onClick={this.openApplicationLink}>
-                  {translate.round_application_link}
-                  <div className='icon-back-arrow'></div>
+                  ? <Button name={translate.round_application_link} color='primary' onClick={this.openApplicationLink}>
+                    {translate.round_application_link}
+                    <div className='icon-back-arrow'></div>
                   </Button>
-                  : ''}
+                  : ''
+                }
               </span>
             : ''}
           </Col>
-       </Row>
+        </Row>
         <Row id='roundContact'>
           <Col>
           {/** *************************************************************************************************************/}
           {/*                                     Round - contact information                                             */}
           {/** *************************************************************************************************************/}
-          {this.props.courseHasRound && this.props.showRoundData ?
-             <span>
-
+          {this.props.courseHasRound && this.props.showRoundData
+            ? <span>
               <h3 className='right-column-header'>{i18n.messages[this.props.language].courseLabels.header_contact}</h3>
 
               <h4>{roundHeader} </h4>
               <p>{selectedRoundHeader}</p>
 
-              {course.course_contact_name !== EMPTY[this.props.language] ?
-                <span>
+              {course.course_contact_name !== EMPTY[this.props.language]
+                ? <span>
                   <h4>{i18n.messages[this.props.language].courseInformation.course_contact_name}</h4>
                   <p>{course.course_contact_name}</p>
                 </span>
-              : ''}
+                : ''}
 
-                <h4>{i18n.messages[this.props.language].courseInformation.course_examiners}</h4>
-                <span dangerouslySetInnerHTML={{ __html: course.course_examiners }}></span>
+              <h4>{i18n.messages[this.props.language].courseInformation.course_examiners}</h4>
+              <span dangerouslySetInnerHTML={{ __html: course.course_examiners }}></span>
 
-                <h4>{translate.round_responsibles}</h4>
-                <span dangerouslySetInnerHTML={{ __html: round.round_responsibles }}></span>
+              <h4>{translate.round_responsibles}</h4>
+              <span dangerouslySetInnerHTML={{ __html: round.round_responsibles }}></span>
 
-                <h4>{translate.round_teacher}</h4>
-                <span dangerouslySetInnerHTML={{ __html: round.round_teacher }}></span>
-              </span>
-              : ''}
+              <h4>{translate.round_teacher}</h4>
+              <span dangerouslySetInnerHTML={{ __html: round.round_teacher }}></span>
+            </span>
+            : ''}
           </Col>
-       </Row>
-
+        </Row>
       </div>
     )
   }
 }
-
-
 
 export default CourseKeyInformationOneCol
