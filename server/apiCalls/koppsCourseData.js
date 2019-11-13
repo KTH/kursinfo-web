683bd5b3293ec5b9998947703f57c7ae97a14e71
Elena Rakhimova <elenara@kth.se>
2019-11-13 13:17:27 +0100

    test koppsApi to use cache

--- server/apiCalls/koppsCourseData.js
+++ server/apiCalls/koppsCourseData.js
@@ -2,20 +2,28 @@
 const log = require('kth-node-log')
 const config = require('../configuration').server
 const BasicAPI = require('kth-node-api-call').BasicAPI
+const redis = require('kth-node-redis')
 
+const cacheConfig = {
+  koppsApi: config.cache.koppsApi
+}
 const koppsApi = new BasicAPI({
+  log,
   hostname: config.koppsApi.host,
   basePath: config.koppsApi.basePath,
   https: true,
   json: true,
   // Kopps is a public API and needs no API-key
   defaultTimeout: config.koppsApi.defaultTimeout,
-  retryOnESOCKETTIMEDOUT: true,
-  redis: {
-    client: config.cache.koppsApi.redis,
-    prefix: 'koppsApi',
-    expire: config.cache.koppsApi.expireTime
-  }
+  // retryOnESOCKETTIMEDOUT: true,
+  timeout: 5000,
+  redis,
+  // {
+  //   client: config.cache.koppsApi.redis,
+  //   prefix: 'koppsApi',
+  //   expire: config.cache.koppsApi.expireTime
+  // },
+  cache: cacheConfig
 })
 
 module.exports = {
@@ -26,7 +34,7 @@ async function getKoppsCourseData (courseCode, lang = 'sv') {
   try {
     return await koppsApi.getAsync({uri: `course/${encodeURIComponent(courseCode)}/detailedinformation?l=${lang}`, useCache: true})
   } catch (err) {
-    log.debug('Kopps is not available')
-    throw err
+    log.debug('Kopps is not available', err)
+    return (err)
   }
 }
