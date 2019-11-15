967c1fc499fe857c6edfec980cfbd2ddf98d6dab
Elena Rakhimova <elenara@kth.se>
2019-11-15 10:52:22 +0100

    changed apis timeouts from 30000 to 5000

--- server/api.js
+++ server/api.js
@@ -9,7 +9,7 @@ const opts = {
   log,
   redis,
   cache: config.cache,
-  timeout: 30000,
+  timeout: 5000,
   retryOnESOCKETTIMEDOUT: true,
   checkAPIs: true // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
 }
