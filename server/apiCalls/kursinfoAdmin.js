605988df6f57c411248f0d522d7f2aa548aaec80
Elena Rakhimova <elenara@kth.se>
2019-11-15 12:28:31 +0100

    remove kursinfo getting paths from api

--- server/apiCalls/kursinfoAdmin.js
+++ server/apiCalls/kursinfoAdmin.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const api = require('../api')
+const config = require('../configuration').server
 
 module.exports = {
   getSellingText: getSellingText
 }
 
 async function getSellingText (courseCode) {
-  const { client, paths } = api.kursinfoApi
-  const uri = client.resolve(paths.getSellingTextByCourseCode.uri, { courseCode })
+  const { client } = api.kursinfoApi
+  const uri = `${config.nodeApi.kursinfoApi.proxyBasePath}/v1/sellingInfo/${encodeURIComponent(courseCode)}`
+  // client.resolve(paths.getSellingTextByCourseCode.uri, { courseCode })
   return await client.getAsync({uri: uri, useCache: true})
 }
