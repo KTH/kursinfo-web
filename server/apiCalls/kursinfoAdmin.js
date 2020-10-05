e961087be2e08c727ec886dc31aaa0b187c25b48
Rickard Falk <rickard.falk@gmail.com>
2020-10-05 10:34:36 +0200

    Retrive data from kursinfo-api

--- server/apiCalls/kursinfoAdmin.js
+++ server/apiCalls/kursinfoAdmin.js
@@ -3,11 +3,11 @@
 const api = require('../api')
 
 module.exports = {
-  getSellingText: getSellingText
+  getSellingText: _getSellingText
 }
 
-async function getSellingText (courseCode) {
+async function _getSellingText(courseCode) {
   const { client, paths } = api.kursinfoApi
   const uri = client.resolve(paths.getSellingTextByCourseCode.uri, { courseCode })
-  return await client.getAsync({uri: uri, useCache: true})
+  return await client.getAsync({ uri: uri, useCache: true })
 }
