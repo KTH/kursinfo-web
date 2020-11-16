f90a629438a97e2fbf7e796a7ce274757b95f1ce
Elena Rakhimova <elenara@kth.se>
2020-11-16 17:47:19 +0100

    fix: handle if memo api is down it will not crush app

--- server/apiCalls/memoApi.js
+++ server/apiCalls/memoApi.js
@@ -1,13 +1,18 @@
 'use strict'
 
 const api = require('../api')
+const log = require('kth-node-log')
 
 async function _getFileList(courseCode) {
-  const { client, paths } = api.kursPmDataApi
-  const uri = client.resolve(paths.getStoredMemoPdfListByCourseCode.uri, {
-    courseCode
-  })
-  return client.getAsync({ uri, useCache: true })
+  try {
+    const { client, paths } = api.kursPmDataApi
+    const uri = client.resolve(paths.getStoredMemoPdfListByCourseCode.uri, {
+      courseCode
+    })
+    return client.getAsync({ uri, useCache: true })
+  } catch (err) {
+    log.error('_getFileList is trying to connect with kurs-pm-api and failed for courseCode: ', courseCode, { err })
+  }
 }
 
 module.exports = {
