de55c2791c2045206a433b513f61e65ca6f22c70
Elena Rakhimova <elenara@kth.se>
2020-11-20 17:42:07 +0100

    feat: add tests for CourseSections

--- public/js/app/components/__tests__/CourseSections.test.js
+++ public/js/app/components/__tests__/CourseSections.test.js
@@ -1,10 +1,77 @@
 import React from 'react'
-import { render } from '@testing-library/react'
 
 import CourseSections from '../CourseSections'
+import i18n from '../../../../../i18n'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+const { getByText, queryByText } = screen
+const translationEN = i18n.messages[0]
 
 describe('Component <CourseSections>', () => {
   test('renders course sections', () => {
     render(<CourseSections />)
+  })
+
+  test('render text with a syllabus marker correctly', () => {
+    const mockData = [{ header: 'First test header', text: 'Text for test from test syllabus', syllabusMarker: true }]
+    render(<CourseSections
+      sectionHeader={'Section with all test headers and texts'}
+      headerType="3"
+      courseData={mockData}
+      sectionId="firstSection"
+      syllabusMarkerAriaLabel={translationEN.courseLabels.syllabus_marker_aria_label}
+    />)
+
+    const header = getByText(mockData[0].header)
+    expect(header).toBeInTheDocument()
+    expect(header.querySelector('sup')).toBeInTheDocument()
+    expect(header.querySelector('sup').querySelector('svg')).toBeInTheDocument()
+    
+    const syllabusText = getByText(mockData[0].text)
+    expect(syllabusText).toBeInTheDocument() 
+  })
+
+    test('render text without a syllabus marker correctly', () => {
+    const mockData = [{ header: 'Test header chosen by user', text: 'Text for test not from syllabys'}]
+    render(<CourseSections
+      sectionHeader={'Section with all test headers and texts'}
+      headerType="3"
+      courseData={mockData}
+      sectionId="freeSection"
+      syllabusMarkerAriaLabel={translationEN.courseLabels.syllabus_marker_aria_label}
+    />
+    )
+    const header = getByText(mockData[0].header)
+    expect(header).toBeInTheDocument()
+    const syllabusText = getByText(mockData[0].text)
+    expect(syllabusText).toBeInTheDocument() 
+    
+    expect(header.querySelector('sup')).toBeNull()
+  
+  })
+
+  test('render array of several headers with text correctly', () => {
+    const mockData = [{ header: 'First test header', text: 'Text for test from test syllabus', syllabusMarker: true },
+    { header: 'Second test header', text: 'Text for test not from syllabys'}
+    ]
+    render(<CourseSections
+      sectionHeader={'Section with all test headers and texts'}
+      headerType="3"
+      courseData={mockData}
+      sectionId="firstSection"
+      syllabusMarkerAriaLabel={translationEN.courseLabels.syllabus_marker_aria_label}
+    />
+    )
+
+    const text1 = getByText(mockData[0].header)
+    expect(text1).toBeInTheDocument()
+    const syllabusText1 = getByText(mockData[0].text)
+    expect(syllabusText1).toBeInTheDocument()
+
+    const text2 = getByText(mockData[1].header)
+    expect(text2).toBeInTheDocument()
+    const syllabusText2 = getByText(mockData[1].text)
+    expect(syllabusText2).toBeInTheDocument() 
+
   })
 })
