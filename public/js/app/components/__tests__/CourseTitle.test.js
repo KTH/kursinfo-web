90674aae50b24133b410b555262bff4d64e1561e
Elena Rakhimova <elenara@kth.se>
2021-12-20 14:47:27 +0100

    text: fix error in Administer Aboute course

--- public/js/app/components/__tests__/CourseTitle.test.js
+++ public/js/app/components/__tests__/CourseTitle.test.js
@@ -10,8 +10,6 @@ const { getByText, queryByText, getByRole } = screen
 const translationEN = i18n.messages[0]
 const translationSV = i18n.messages[1]
 
-
-
 describe('Component <CourseTitle>', () => {
   test('renders a course title', () => {
     render(<CourseTitle />)
@@ -24,16 +22,17 @@ describe('Component <CourseTitle>', () => {
       course_credits: '9',
       course_credits_text: 'hp'
     }
-    render(<CourseTitle
-      key="title"
-      courseTitleData={courseTitleData}
-      language={'en'}
-      pageTitle={translationEN.courseLabels.sideMenu.page_before_course}
-    />)
+    render(
+      <CourseTitle
+        key="title"
+        courseTitleData={courseTitleData}
+        language={'en'}
+        pageTitle={translationEN.courseLabels.sideMenu.page_before_course}
+      />
+    )
 
     const pageTitle = queryByText('Before course selection')
     expect(pageTitle).toBeInTheDocument()
-
 
     const courseTitle = queryByText('KIP1111 Project in Plasma Physics 9.0 credits')
     expect(courseTitle).toBeInTheDocument()
@@ -42,11 +41,10 @@ describe('Component <CourseTitle>', () => {
     expect(adminLink).toBeInTheDocument()
     expect(adminLink.href).toBe('http://localhost/kursinfoadmin/kurser/kurs/KIP1111?l=en')
 
-    const adminLinkName = queryByText('Administer Aboute course')
+    const adminLinkName = queryByText('Administer About course')
     expect(adminLinkName).toBeInTheDocument()
 
     done()
-
   })
 
   test('renders a course title with data and use correct credit unit in Swedish', (done) => {
@@ -56,16 +54,17 @@ describe('Component <CourseTitle>', () => {
       course_credits: '9',
       course_credits_text: 'hp'
     }
-    render(<CourseTitle
-      key="title"
-      courseTitleData={courseTitleData}
-      language={'sv'}
-      pageTitle={translationSV.courseLabels.sideMenu.page_before_course}
-    />)
+    render(
+      <CourseTitle
+        key="title"
+        courseTitleData={courseTitleData}
+        language={'sv'}
+        pageTitle={translationSV.courseLabels.sideMenu.page_before_course}
+      />
+    )
 
     const pageTitle = queryByText('Inför kursval')
     expect(pageTitle).toBeInTheDocument()
-
 
     const courseTitle = queryByText('KIP1111 Projekt i plasmafysik 9,0 hp')
     expect(courseTitle).toBeInTheDocument()
