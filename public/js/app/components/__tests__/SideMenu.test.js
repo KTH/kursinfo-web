77ccd60d62b2f3dd05e606042c269bf5bea9f1af
Elena Rakhimova <elenara@kth.se>
2022-03-18 11:21:12 +0100

    test: fix url in test

--- public/js/app/components/__tests__/SideMenu.test.js
+++ public/js/app/components/__tests__/SideMenu.test.js
@@ -16,19 +16,15 @@ describe('Component <SideMenu>', () => {
   })
 
   test('renders a side menu for a course and with labels. English', () => {
-    render(<SideMenu
-      courseCode={'KIP1111'}
-      labels={translationEN.courseLabels.sideMenu}
-      language={'en'}
-    />)
+    render(<SideMenu courseCode={'KIP1111'} labels={translationEN.courseLabels.sideMenu} language={'en'} />)
 
     const links = getAllByRole('link')
     expect(links.length).toBe(5)
 
-    expect(links[0].href).toBe('https://www.kth.se/student/kurser/kurser-inom-program?l=en')
+    expect(links[0].href).toBe('http://localhost/student/kurser/kurser-inom-program?l=en')
     expect(links[0].title).toBe('Course and programme directory')
 
-    expect(links[1].href).toBe('https://www.kth.se/student/kurser/kurs/KIP1111?l=en')
+    expect(links[1].href).toBe('http://localhost/student/kurser/kurs/KIP1111?l=en')
     expect(links[1].title).toBe('Before course selection')
 
     expect(links[2].href).toBe('http://localhost/kurs-pm/KIP1111?l=en')
@@ -41,23 +37,18 @@ describe('Component <SideMenu>', () => {
     expect(links[4].title).toBe('Archive')
 
     expect(queryByText('About course KIP1111')).toBeInTheDocument()
-
   })
 
-    test('renders a side menu for a course and with labels. Swedish', () => {
-    render(<SideMenu
-      courseCode={'KIP1111'}
-      labels={translationSV.courseLabels.sideMenu}
-      language={'sv'}
-    />)
+  test('renders a side menu for a course and with labels. Swedish', () => {
+    render(<SideMenu courseCode={'KIP1111'} labels={translationSV.courseLabels.sideMenu} language={'sv'} />)
 
     const links = getAllByRole('link')
     expect(links.length).toBe(5)
 
-    expect(links[0].href).toBe('https://www.kth.se/student/kurser/kurser-inom-program')
+    expect(links[0].href).toBe('http://localhost/student/kurser/kurser-inom-program')
     expect(links[0].title).toBe('Kurs- och programkatalogen')
 
-    expect(links[1].href).toBe('https://www.kth.se/student/kurser/kurs/KIP1111')
+    expect(links[1].href).toBe('http://localhost/student/kurser/kurs/KIP1111')
     expect(links[1].title).toBe('Inför kursval')
 
     expect(links[2].href).toBe('http://localhost/kurs-pm/KIP1111')
@@ -70,9 +61,5 @@ describe('Component <SideMenu>', () => {
     expect(links[4].title).toBe('Arkiv')
 
     expect(queryByText('Om kursen KIP1111')).toBeInTheDocument()
-
   })
 })
-
-
-
