c6d0bb4c4d837f5f86837362b730a79dadba97b4
Elena Rakhimova <elenara@kth.se>
2022-09-15 09:44:07 +0200

    feat (KUI-783): combine all components in index

---
+++ public/js/app/components/statistics/index.js
@@ -0,0 +1,16 @@
+import CheckboxOption from './CheckboxOption'
+import RadioboxOption from './RadioboxOption'
+import StatisticsAlert from './StatisticsAlert'
+import StatisticsForm from './StatisticsForm'
+import StatisticsResults from './StatisticsResults'
+import { englishTexts, swedishTexts } from './StatisticsTexts'
+
+export {
+  CheckboxOption,
+  englishTexts,
+  RadioboxOption,
+  StatisticsAlert,
+  StatisticsForm,
+  StatisticsResults,
+  swedishTexts,
+}
