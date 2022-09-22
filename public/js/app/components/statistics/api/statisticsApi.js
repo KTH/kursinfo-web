b396e70c31324761e0459b4c44dd6b2a3b47af39
Elena Rakhimova <elenara@kth.se>
2022-09-22 14:54:46 +0200

    fix  (KUI-790): send back error if some of parameters are missing

--- public/js/app/components/statistics/api/statisticsApi.js
+++ public/js/app/components/statistics/api/statisticsApi.js
@@ -3,9 +3,15 @@ import axios from 'axios'
 // eslint-disable-next-line consistent-return
 async function fetchStatistics(language, proxyUrl, params) {
   try {
+    const { documentType, year } = params
+    const values = Object.values(params)
+
+    if (values.length < 4 || values.includes('undefined') || values.includes('') || values.includes('null'))
+      return 'missing-parameters-in-query'
+
     // To-do send all data as a string documentType and year in url so we can see it in
     // Application insights
-    const { documentType, year } = params
+
     const url = `${proxyUrl}/api/kursinfo/statistics/${documentType}/${year}/${language}`
     const result = await axios.get(
       url /* , {
