e8f56580a85820e5392bab69dcfca5debe7a18e8
Elena Rakhimova <elenara@kth.se>
2022-09-22 14:53:44 +0200

    text (KUI-760): add page introduction text in Swedish

--- public/js/app/components/statistics/StatisticsTexts.jsx
+++ public/js/app/components/statistics/StatisticsTexts.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 const englishTexts = {
   pageHeader: `Course Information Statistics`,
   pageDescription: () => (
+    // in use
     <>
       <p> ??? Description in english</p>
     </>
@@ -121,8 +122,26 @@ const englishTexts = {
 }
 const swedishTexts = {
   pageDescription: () => (
+    // in use
     <>
-      <p>??? Description in swedish </p>
+      <p>
+        Statistiksidan för kursinformation är en del av verktyget Administrera Om kursen. De kurs-PM och kursanalyser
+        som publiceras via verktyget utgör underlaget för statistiken. Läs mer om Om kursen på intranätet.{' '}
+      </p>
+      <p>
+        Statistiken visas områdesvis för kurs-PM respektive kursanalys. Det går att se statistik för en eller alla
+        skolor samt för en eller flera läsperioder/terminer. Det finns statistik att hämta från år 2019 och framåt.
+        Statistiken för kurs-PM möjliggör uppföljning på skolnivå utifrån KTH:s riktlinje för kurs-PM.
+      </p>
+      <p>På statistiksidan finns:</p>
+      <ul>
+        <li>en sammanställning av antalet publicerade kurs-PM/kursanalyser för vald skola och läsperiod/termin</li>
+        <li>möjligheten att jämföra data för det valda året med statistik från föregående år</li>
+        <li>
+          en tabell med den valda skolans samtliga kurser för vald läsperiod/termin, vilken går att ladda ner för att
+          t.ex. göra filtreringar och sorteringar på programnivå
+        </li>
+      </ul>
     </>
   ),
   subHeader: 'Per School', // move to messages.en/.se
