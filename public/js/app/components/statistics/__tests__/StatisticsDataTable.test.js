6e2a6e51b1ef6d567e7689ff804dcdb8e09b44df
Rao-Sabahat-Ali <raosabahat01@live.com>
2022-10-17 11:18:00 +0200

    test: Fix test case mock up data

--- public/js/app/components/statistics/__tests__/StatisticsDataTable.test.js
+++ public/js/app/components/statistics/__tests__/StatisticsDataTable.test.js
@@ -4,31 +4,50 @@ import '@testing-library/jest-dom/extend-expect'
 import { WebContextProvider } from '../../../context/WebContext'
 import StatisticsDataTable from '../StatisticsDataTable'
 
-const data = [
-  {
-    year: '2022',
-    school: 'IT',
-    institution: 'Energiteknik',
-    courseCode: 'SF1624',
-    linkedProgram: 'CDEPR-2, CINEK-EHUI-2, CITEH-2',
-    courseRoundNumber: '1',
-    semester: '4',
-    courseStart: '2022-03-21',
-    publishDate: '2022-01-10',
-    linkToCourse: null,
-  },
-]
+const statisticsResult = {
+  year: 2019,
+  offeringsWithMemos: [
+    {
+      endDate: '2020-01-14',
+      firstSemester: '20192',
+      startDate: '2019-08-26',
+      schoolMainCode: 'ABE',
+      departmentName: 'ABE/CDE, Gru 1-2-3',
+      connectedPrograms: 'ARKIT-3',
+      courseCode: 'A31REA',
+      offeringId: '1',
+      period: 'P1',
+      courseMemoInfo: {
+        courseCode: 'A31REA',
+        courseMemoFileName: 'memo-123-3456.pdf',
+        ladokRoundIds: ['1'],
+        semester: '20192',
+        isPdf: true,
+        lastChangeDate: 'Wed Dec 04 2019 22:36:39 GMT+0000 (Coordinated Universal Time)',
+        publishedData: {
+          offeringStartTime: '2019-08-26',
+          publishedTime: '2019-12-04',
+          publishedBeforeStart: false,
+          publishedBeforeDeadline: false,
+        },
+      },
+    },
+  ],
+}
 const language_en = 'en'
 
 const context_en = {
   language: language_en,
   languageIndex: language_en === 'en' ? 0 : 1,
+  browserConfig: {
+    hostUrl: 'http://localhost:3000',
+  },
 }
 
 function StatisticsDataTableWithContext({ context }) {
   return (
     <WebContextProvider configIn={context}>
-      <StatisticsDataTable data={data} />
+      <StatisticsDataTable statisticsResult={statisticsResult} />
     </WebContextProvider>
   )
 }
@@ -36,7 +55,7 @@ function StatisticsDataTableWithContext({ context }) {
 function StatisticsDataTableWithContextWithOutData({ context }) {
   return (
     <WebContextProvider configIn={context}>
-      <StatisticsDataTable data={[]} />
+      <StatisticsDataTable statisticsResult={{}} />
     </WebContextProvider>
   )
 }
@@ -49,10 +68,10 @@ describe('Component <StatisticsDataTable> in english', () => {
 
   test('Renders a table for a statistics page', () => {
     render(<StatisticsDataTableWithContext context={context_en} />)
-    expect(screen.getByText('Energiteknik')).toBeInTheDocument()
-    expect(screen.getByText('SF1624')).toBeInTheDocument()
-    expect(screen.getByText('CDEPR-2, CINEK-EHUI-2, CITEH-2')).toBeInTheDocument()
-    expect(screen.getByText('2022-03-21')).toBeInTheDocument()
-    expect(screen.getByText('2022-01-10')).toBeInTheDocument()
+    expect(screen.getByText('A31REA')).toBeInTheDocument()
+    expect(screen.getByText('ABE/CDE, Gru 1-2-3')).toBeInTheDocument()
+    expect(screen.getByText('memo-123-3456.pdf')).toBeInTheDocument()
+    expect(screen.getByText('ABE')).toBeInTheDocument()
+    expect(screen.getByText('2019-08-26')).toBeInTheDocument()
   })
 })
