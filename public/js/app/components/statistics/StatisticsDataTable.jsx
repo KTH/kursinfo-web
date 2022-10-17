034bdb4a2d6228f3b6fd78a2ae7e84b2025e4baf
Rao-Sabahat-Ali <raosabahat01@live.com>
2022-10-17 17:05:29 +0200

    perf: Remove useEffect and change columns implementation

--- public/js/app/components/statistics/StatisticsDataTable.jsx
+++ public/js/app/components/statistics/StatisticsDataTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useWebContext } from '../../context/WebContext'
 import i18n from '../../../../../i18n'
 import SortableDataTable, { useRowsPerPage } from './SortableDataTable'
@@ -14,124 +14,75 @@ function StatisticsDataTable({ statisticsResult }) {
   const { sortableTable } = statisticsLabels
   const { statisticsDataColumns } = sortableTable
   const { readRowsPerPage, writeRowsPerPage } = useRowsPerPage('statisticsPage', 'EditableDataTableForStatistics', '10')
-  const [tableData, setTableData] = useState([])
-
-  const columns = [
-    {
-      name: statisticsDataColumns.year,
-      selector: group => group.year,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.school,
-      selector: group => group.school,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.institution,
-      selector: group => group.institution,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.courseCode,
-      selector: group => group.courseCode,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.linkedProgram,
-      selector: group => group.linkedProgram,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.courseRoundNumber,
-      selector: group => group.courseRoundNumber,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.semester,
-      selector: group => group.semester,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.courseStart,
-      selector: group => group.courseStart,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.publishDate,
-      selector: group => group.publishDate,
-      sortable: true,
-      wrap: true,
-    },
-    {
-      name: statisticsDataColumns.linkToCourse,
-      selector: group => group.linkToCourse,
-      sortable: true,
-      wrap: true,
-    },
+  let tableData = []
+  const columnsNames = [
+    'year',
+    'school',
+    'institution',
+    'courseCode',
+    'linkedProgram',
+    'courseRoundNumber',
+    'semester',
+    'courseStart',
+    'publishDate',
+    'linkToCourse',
   ]
+  const columns = columnsNames.map(columnName => ({
+    name: statisticsDataColumns[columnName],
+    selector: group => group[columnName],
+    sortable: true,
+    wrap: true,
+  }))
 
   const buildLink = (link, textToShow) => <a href={`${link}`}>{textToShow}</a>
 
-  useEffect(() => {
-    if (statisticsResult && statisticsResult.offeringsWithMemos && statisticsResult.offeringsWithMemos.length > 0) {
-      const dataRows = []
-      statisticsResult.offeringsWithMemos.forEach(offeringWithMemos => {
-        if (offeringWithMemos.courseMemoInfo && Object.keys(offeringWithMemos.courseMemoInfo).length > 0) {
-          const { courseMemoFileName, memoEndPoint } = offeringWithMemos.courseMemoInfo
-          const memoId = courseMemoFileName || memoEndPoint
-          offeringWithMemos.courseMemoInfo.ladokRoundIds.forEach(roundId => {
-            dataRows.push({
-              year: statisticsResult.year,
-              school: offeringWithMemos.schoolMainCode,
-              institution: offeringWithMemos.departmentName,
-              courseCode: offeringWithMemos.courseCode,
-              linkedProgram: offeringWithMemos.connectedPrograms,
-              courseRoundNumber: roundId,
-              semester: offeringWithMemos.courseMemoInfo.semester,
-              courseStart: offeringWithMemos.startDate,
-              publishDate: offeringWithMemos.courseMemoInfo.offeringStartTime,
-              linkToCourse: offeringWithMemos.courseMemoInfo.isPdf
-                ? buildLink(browserConfig.memoStorageUri, `${offeringWithMemos.courseMemoInfo && memoId}`)
-                : buildLink(
-                    `${_getThisHost(browserConfig.hostUrl)}/kurs-pm/${offeringWithMemos.courseCode}/${
-                      offeringWithMemos.courseMemoInfo && memoId
-                    }`,
-                    `${offeringWithMemos.courseMemoInfo && memoId}`
-                  ),
-            })
-          })
-        } else {
+  if (statisticsResult && statisticsResult.offeringsWithMemos && statisticsResult.offeringsWithMemos.length > 0) {
+    const dataRows = []
+    statisticsResult.offeringsWithMemos.forEach(offeringWithMemos => {
+      if (offeringWithMemos.courseMemoInfo && Object.keys(offeringWithMemos.courseMemoInfo).length > 0) {
+        const { courseMemoFileName, memoEndPoint } = offeringWithMemos.courseMemoInfo
+        const memoId = courseMemoFileName || memoEndPoint
+        offeringWithMemos.courseMemoInfo.ladokRoundIds.forEach(roundId => {
           dataRows.push({
             year: statisticsResult.year,
             school: offeringWithMemos.schoolMainCode,
             institution: offeringWithMemos.departmentName,
             courseCode: offeringWithMemos.courseCode,
             linkedProgram: offeringWithMemos.connectedPrograms,
-            courseRoundNumber: '',
-            semester: offeringWithMemos.firstSemester,
+            courseRoundNumber: roundId,
+            semester: offeringWithMemos.courseMemoInfo.semester,
             courseStart: offeringWithMemos.startDate,
-            publishDate: '',
-            linkToCourse: '',
+            publishDate: offeringWithMemos.courseMemoInfo.offeringStartTime,
+            linkToCourse: offeringWithMemos.courseMemoInfo.isPdf
+              ? buildLink(browserConfig.memoStorageUri, `${offeringWithMemos.courseMemoInfo && memoId}`)
+              : buildLink(
+                  `${_getThisHost(browserConfig.hostUrl)}/kurs-pm/${offeringWithMemos.courseCode}/${
+                    offeringWithMemos.courseMemoInfo && memoId
+                  }`,
+                  `${offeringWithMemos.courseMemoInfo && memoId}`
+                ),
           })
-        }
-      })
-      setTableData(
-        dataRows.map((d, index) => ({
-          id: index,
-          ...d,
-        }))
-      )
-    }
-  }, [statisticsResult])
+        })
+      } else {
+        dataRows.push({
+          year: statisticsResult.year,
+          school: offeringWithMemos.schoolMainCode,
+          institution: offeringWithMemos.departmentName,
+          courseCode: offeringWithMemos.courseCode,
+          linkedProgram: offeringWithMemos.connectedPrograms,
+          courseRoundNumber: '',
+          semester: offeringWithMemos.firstSemester,
+          courseStart: offeringWithMemos.startDate,
+          publishDate: '',
+          linkToCourse: '',
+        })
+      }
+    })
+    tableData = dataRows.map((d, index) => ({
+      id: index,
+      ...d,
+    }))
+  }
 
   return tableData.length === 0 ? (
     <p>
