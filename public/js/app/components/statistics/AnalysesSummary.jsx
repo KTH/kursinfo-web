7ec058a7893332fafa32aa59d033aceb4eb6ace5
Elena Rakhimova <elenara@kth.se>
2022-10-20 15:35:06 +0200

    refactor: use common component for memos and anlyses tables

--- public/js/app/components/statistics/AnalysesSummary.jsx
+++ public/js/app/components/statistics/AnalysesSummary.jsx
@@ -4,56 +4,31 @@ import PropTypes from 'prop-types'
 
 import i18n from '../../../../../i18n'
 import { useWebContext } from '../../context/WebContext'
-import { summaryTexts } from './StatisticsTexts'
+import { TableSummary } from './TableSummaryRows'
+
+function getSchoolNumbers(school) {
+  const { numberOfCourses, numberOfUniqAnalyses } = school
+  return [numberOfCourses, numberOfUniqAnalyses]
+}
+function getFootTotalNumbers(totals) {
+  return [totals.totalCourses, totals.totalUniqPublishedAnalyses]
+}
 
 function AnalysesNumbersTable({ statisticsResult }) {
   const [{ languageIndex }] = useWebContext()
-  const { statisticsLabels: labels } = i18n.messages[languageIndex]
-  const { analysesNumbersTable: tableLabels } = labels.summaryLabels
+  const { summaryLabels } = i18n.messages[languageIndex].statisticsLabels
+  const { analysesNumbersTable } = summaryLabels
   const { combinedAnalysesPerSchool } = statisticsResult
-  const headersLabelsIds = ['school', 'totalCourses', 'totalPublishedAnalyses']
-  const analysesPerSchoolRows = () => {
-    const rows = []
-    const { schools = {}, totalNumberOfCourses, totalNumberOfUniqAnalyses } = combinedAnalysesPerSchool
-    Object.keys(schools).forEach(sC => {
-      const { numberOfCourses, numberOfUniqAnalyses } = schools[sC]
-      rows.push(
-        <tr key={sC}>
-          <td>{sC}</td>
-          <td>{numberOfCourses}</td>
-          <td>{numberOfUniqAnalyses}</td>
-        </tr>
-      )
-    })
-    if (Object.keys(schools) > 1)
-      rows.push(
-        <tr key="totals">
-          <td>
-            <b>
-              <i>Total</i>
-            </b>
-          </td>
-          <td>
-            <b>{totalNumberOfCourses}</b>
-          </td>
-          <td>
-            <b>{totalNumberOfUniqAnalyses}</b>
-          </td>
-        </tr>
-      )
-    return rows
-  }
+  const cellNames = ['totalCourses', 'totalUniqPublishedAnalyses']
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          {headersLabelsIds.map(thId => (
-            <th key={thId}>{tableLabels[thId]}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>{analysesPerSchoolRows()}</tbody>
-    </table>
+    <TableSummary
+      docsPerSchool={combinedAnalysesPerSchool}
+      cellNames={cellNames}
+      getNumbersFn={getSchoolNumbers}
+      labels={analysesNumbersTable}
+      totalNumbers={getFootTotalNumbers(combinedAnalysesPerSchool)}
+    />
   )
 }
 
--- public/js/app/components/statistics/MemosSummary.jsx
+++ public/js/app/components/statistics/MemosSummary.jsx
@@ -1,98 +1,59 @@
 import React from 'react'
 
 import PropTypes from 'prop-types'
-import { Col, Row } from 'reactstrap'
-
-import i18n from '../../../../../i18n'
 import { useWebContext } from '../../context/WebContext'
+import i18n from '../../../../../i18n'
 import { summaryTexts } from './StatisticsTexts'
+import { TableSummary } from './TableSummaryRows'
 
-// import { periods, schools, seasons } from './domain/index'
-// import { DOCS, DOCUMENT_TYPES } from './domain/formConfigurations'
+function getSchoolNumbers(school = {}) {
+  return [
+    school.numberOfCourses,
+    school.numberOfUniqWebMemos + school.numberOfUniqPdfMemos,
+    school.numberOfUniqWebMemos,
+    school.numberOfUniqPdfMemos,
+    school.numberOfMemosPublishedBeforeStart,
+    school.numberOfMemosPublishedBeforeDeadline,
+  ]
+}
 
-function MemosNumbersTable({ statisticsResult }) {
-  const [{ languageIndex }] = useWebContext()
-  const { statisticsLabels: labels } = i18n.messages[languageIndex]
-  const { memosNumbersTable: tableLabels } = labels.summaryLabels
-  const { combinedMemosPerSchool } = statisticsResult
-  const headersLabelsIds = [
-    'school',
+function getFootTotalNumbers(totals) {
+  return [
+    totals.totalCourses,
+    Math.abs(totals.totalNumberOfWebMemos) + Math.abs(totals.totalNumberOfPdfMemos),
+    totals.totalNumberOfWebMemos,
+    totals.totalNumberOfPdfMemos,
+    totals.totalNumberOfMemosPublishedBeforeStart,
+    totals.totalNumberOfMemosPublishedBeforeDeadline,
+  ]
+}
+
+function getCellNames() {
+  return [
     'totalCourses',
     'totalPublishedMemos',
-    'totalPublWeMemos',
-    'totalPublPdMemos',
+    'totalPublWebMemos',
+    'totalPublPdfMemos',
     'totalMemosPublishedBeforeCourseStart',
     'totalMemosPublishedBeforeDeadline',
   ]
-  const memosPerSchoolRows = () => {
-    const rows = []
-    const {
-      schools = {},
-      totalNumberOfCourses,
-      totalNumberOfWebMemos,
-      totalNumberOfPdfMemos,
-      totalNumberOfMemosPublishedBeforeStart,
-      totalNumberOfMemosPublishedBeforeDeadline,
-    } = combinedMemosPerSchool
-    Object.keys(schools).forEach(sC => {
-      const {
-        numberOfCourses,
-        numberOfUniqWebMemos,
-        numberOfUniqPdfMemos,
-        numberOfMemosPublishedBeforeStart,
-        numberOfMemosPublishedBeforeDeadline,
-      } = schools[sC]
-      rows.push(
-        <tr key={sC}>
-          <td>{sC}</td>
-          <td>{numberOfCourses}</td>
-          <td>{numberOfUniqWebMemos + numberOfUniqPdfMemos}</td>
-          <td>{numberOfUniqWebMemos}</td>
-          <td>{numberOfUniqPdfMemos}</td>
-          <td>{numberOfMemosPublishedBeforeStart}</td>
-          <td>{numberOfMemosPublishedBeforeDeadline}</td>
-        </tr>
-      )
-    })
-    if (Object.keys(schools) > 1)
-      rows.push(
-        <tr key="totals">
-          <td>
-            <b>
-              <i>Total</i>
-            </b>
-          </td>
-          <td>
-            <b>{totalNumberOfCourses}</b>
-          </td>
-          <td>{Math.abs(totalNumberOfWebMemos) + Math.abs(totalNumberOfPdfMemos)}</td>
-          <td>
-            <b>{totalNumberOfWebMemos}</b>
-          </td>
-          <td>
-            <b>{totalNumberOfPdfMemos}</b>
-          </td>
-          <td>
-            <b>{totalNumberOfMemosPublishedBeforeStart}</b>
-          </td>
-          <td>
-            <b>{totalNumberOfMemosPublishedBeforeDeadline}</b>
-          </td>
-        </tr>
-      )
-    return rows
-  }
+}
+
+function MemosNumbersTable({ statisticsResult }) {
+  const [{ languageIndex }] = useWebContext()
+  const { summaryLabels } = i18n.messages[languageIndex].statisticsLabels
+  const { memosNumbersTable } = summaryLabels
+  const { combinedMemosPerSchool } = statisticsResult
+  const cellNames = getCellNames()
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          {headersLabelsIds.map(thId => (
-            <th key={thId}>{tableLabels[thId]}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>{memosPerSchoolRows()}</tbody>
-    </table>
+    <TableSummary
+      docsPerSchool={combinedMemosPerSchool}
+      cellNames={cellNames}
+      getNumbersFn={getSchoolNumbers}
+      labels={memosNumbersTable}
+      totalNumbers={getFootTotalNumbers(combinedMemosPerSchool)}
+    />
   )
 }
 
