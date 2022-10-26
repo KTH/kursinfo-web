c35dc8ad2dd5e78c3660f43b9df04cbadbadfadc
Rao-Sabahat-Ali <raosabahat01@live.com>
2022-10-26 11:07:19 +0200

    test: Add test cases for export and analysis table

--- public/js/app/components/statistics/__tests__/StatisticsDataTable.test.js
+++ public/js/app/components/statistics/__tests__/StatisticsDataTable.test.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import { WebContextProvider } from '../../../context/WebContext'
 import StatisticsDataTable from '../StatisticsDataTable'
 
-const statisticsResult = {
+const statisticsResultForMemo = {
   year: 2019,
+  periods: ['1'],
   offeringsWithMemos: [
     {
       endDate: '2020-01-14',
@@ -34,6 +36,33 @@ const statisticsResult = {
     },
   ],
 }
+
+const statisticsResultForAnalysis = {
+  year: 2019,
+  seasons: ['1'],
+  offeringsWithAnalyses: [
+    {
+      endDate: '2020-01-14',
+      firstSemester: '20192',
+      startDate: '2019-08-26',
+      schoolMainCode: 'ABE',
+      departmentName: 'ABE/CDE, Gru 1-2-3',
+      connectedPrograms: 'ARKIT-3',
+      courseCode: 'A31REA',
+      offeringId: '1',
+      period: 'P1',
+      courseAnalysisInfo: {
+        courseCode: 'A31REA',
+        analysisFileName: 'analysis-123-3456.pdf',
+        ladokRoundIds: ['1'],
+        semester: '20192',
+        isPublished: true,
+        lastChangeDate: 'Wed Dec 04 2019 22:36:39 GMT+0000 (Coordinated Universal Time)',
+        publishedDate: '2019-12-04',
+      },
+    },
+  ],
+}
 const language_en = 'en'
 
 const context_en = {
@@ -44,7 +73,7 @@ const context_en = {
   },
 }
 
-function StatisticsDataTableWithContext({ context }) {
+function StatisticsDataTableWithContext({ context, statisticsResult }) {
   return (
     <WebContextProvider configIn={context}>
       <StatisticsDataTable statisticsResult={statisticsResult} />
@@ -66,12 +95,33 @@ describe('Component <StatisticsDataTable> in english', () => {
     expect(screen.getByText('No statistics data found!')).toBeInTheDocument()
   })
 
-  test('Renders a table for a statistics page', () => {
-    render(<StatisticsDataTableWithContext context={context_en} />)
+  test('Renders a table for a statistics page for course memo', () => {
+    render(<StatisticsDataTableWithContext context={context_en} statisticsResult={statisticsResultForMemo} />)
     expect(screen.getByText('A31REA')).toBeInTheDocument()
     expect(screen.getByText('ABE/CDE, Gru 1-2-3')).toBeInTheDocument()
     expect(screen.getByText('memo-123-3456.pdf')).toBeInTheDocument()
     expect(screen.getByText('ABE')).toBeInTheDocument()
     expect(screen.getByText('2019-08-26')).toBeInTheDocument()
+    const csvDownloadButton = screen.getByRole('button', { name: /Download table as csv file/i })
+    expect(csvDownloadButton).toBeInTheDocument()
+    userEvent.click(csvDownloadButton)
+    const excelDownloadButton = screen.getByRole('button', { name: /Download table as excel file/i })
+    expect(excelDownloadButton).toBeInTheDocument()
+    userEvent.click(excelDownloadButton)
+  })
+
+  test('Renders a table for a statistics page for course analysis', () => {
+    render(<StatisticsDataTableWithContext context={context_en} statisticsResult={statisticsResultForAnalysis} />)
+    expect(screen.getByText('A31REA')).toBeInTheDocument()
+    expect(screen.getByText('ABE/CDE, Gru 1-2-3')).toBeInTheDocument()
+    expect(screen.getByText('analysis-123-3456.pdf')).toBeInTheDocument()
+    expect(screen.getByText('ABE')).toBeInTheDocument()
+    expect(screen.getByText('2020-01-14')).toBeInTheDocument()
+    const csvDownloadButton = screen.getByRole('button', { name: /Download table as csv file/i })
+    expect(csvDownloadButton).toBeInTheDocument()
+    userEvent.click(csvDownloadButton)
+    const excelDownloadButton = screen.getByRole('button', { name: /Download table as excel file/i })
+    expect(excelDownloadButton).toBeInTheDocument()
+    userEvent.click(excelDownloadButton)
   })
 })
