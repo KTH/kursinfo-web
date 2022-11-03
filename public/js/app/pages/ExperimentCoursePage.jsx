6c0d0d6b8fd94d01ccd105cc1b4176d5591d76cf
Elena Rakhimova <elenara@kth.se>
2022-11-03 12:26:16 +0100

    chore : remove most of components

--- public/js/app/pages/ExperimentCoursePage.jsx
+++ public/js/app/pages/ExperimentCoursePage.jsx
@@ -18,24 +18,24 @@ import { useWebContext } from '../context/WebContext'
 
 const aboutCourseStr = (translate, courseCode = '') => `${translate.site_name} ${courseCode}`
 
-const Breadcrumbs = ({ translation, language, courseCode }) => (
-  <Breadcrumb lang={language} aria-label={translation.breadCrumbLabels.breadcrumbs} className="secondaryMenu">
-    <BreadcrumbItem>
-      <a href={`${breadcrumbLinks.university[language]}`}>{translation.breadCrumbLabels.university}</a>
-    </BreadcrumbItem>
-    <BreadcrumbItem>
-      <a href={`${breadcrumbLinks.student[language]}`}>{translation.breadCrumbLabels.student}</a>
-    </BreadcrumbItem>
-    <BreadcrumbItem>
-      <a href={`${breadcrumbLinks.directory[language]}`}>{translation.breadCrumbLabels.directory}</a>
-    </BreadcrumbItem>
-    <BreadcrumbItem>
-      <a
-        href={`${aboutCourseLink(courseCode, language)}`}
-      >{`${translation.breadCrumbLabels.aboutCourse} ${courseCode}`}</a>
-    </BreadcrumbItem>
-  </Breadcrumb>
-)
+// const Breadcrumbs = ({ translation, language, courseCode }) => (
+//   <Breadcrumb lang={language} aria-label={translation.breadCrumbLabels.breadcrumbs} className="secondaryMenu">
+//     <BreadcrumbItem>
+//       <a href={`${breadcrumbLinks.university[language]}`}>{translation.breadCrumbLabels.university}</a>
+//     </BreadcrumbItem>
+//     <BreadcrumbItem>
+//       <a href={`${breadcrumbLinks.student[language]}`}>{translation.breadCrumbLabels.student}</a>
+//     </BreadcrumbItem>
+//     <BreadcrumbItem>
+//       <a href={`${breadcrumbLinks.directory[language]}`}>{translation.breadCrumbLabels.directory}</a>
+//     </BreadcrumbItem>
+//     <BreadcrumbItem>
+//       <a
+//         href={`${aboutCourseLink(courseCode, language)}`}
+//       >{`${translation.breadCrumbLabels.aboutCourse} ${courseCode}`}</a>
+//     </BreadcrumbItem>
+//   </Breadcrumb>
+// )
 
 function ExperimentCoursePage() {
   const [context, setWebContext] = useWebContext()
@@ -130,9 +130,9 @@ function ExperimentCoursePage() {
 
   return (
     <div key="kursinfo-container" className="col" id="kursinfo-main-page">
-      <Row>
+      {/* <Row>
         <Breadcrumbs translation={translation} language={language} courseCode={courseCode} />
-      </Row>
+      </Row> */}
       <Row id="pageContainer" key="pageContainer">
         <SideMenu courseCode={courseCode} labels={translation.courseLabels.sideMenu} language={language} />
         <main className="col" id="mainContent">
@@ -173,143 +173,48 @@ function ExperimentCoursePage() {
           >
             <Col>
               <img className="float-md-left" src={courseImage} alt="" height="auto" width="300px" />
-              <div className="paragraphs" dangerouslySetInnerHTML={{ __html: introText }} />
+              {/* <div className="paragraphs" dangerouslySetInnerHTML={{ __html: introText }} /> */}
             </Col>
           </section>
-          <Row id="columnContainer" key="columnContainer">
-            <Col id="leftContainer" key="leftContainer">
-              {/** ************************************************************************************************************ */}
-              {/*                                      RIGHT COLUMN - ROUND INFORMATION                                         */}
-              {/** ************************************************************************************************************ */}
-              <Col id="roundInformationContainer" md="4" xs="12" className="float-md-right">
-                {/* ---COURSE  DROPDOWN MENU--- */}
-                {/* ---COURSE DELETED  DROPDOWN MENU--- */}
+          <div>
+            <h2>What is Lorem Ipsum?</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+              of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
 
-                {courseInfo.course_application_info.length > 0 && (
-                  <Alert color="info">
-                    <h4>{translation.courseInformation.course_application_info}</h4>
-                    <span dangerouslySetInnerHTML={{ __html: courseInfo.course_application_info }} />
-                  </Alert>
-                )}
+            <h2>Why do we use it?</h2>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when
+              looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
+              letters, as opposed to using 'Content here, content here', making it look like readable English. Many
+              desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
+              search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
+              over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </p>
 
-                {/* ---COURSE ROUND INFORMATION--- */}
-                {hasActiveSemesters ? (
-                  <RoundInformationOneCol
-                    courseRound={courseData.roundList[activeSemester][activeRoundIndex]}
-                    courseData={courseInformationToRounds}
-                    language={language}
-                    courseHasRound={activeSemesters.length > 0}
-                    fade={context.roundInfoFade}
-                    showRoundData={hasToShowRoundsData}
-                    memoStorageURI={browserConfig.memoStorageUri}
-                  />
-                ) : (
-                  <div className="key-info">
-                    {hasActiveSemesters ? (
-                      <p>{translation.courseLabels.no_round_selected}</p>
-                    ) : (
-                      <i>{translation.courseLabels.lable_no_rounds}</i>
-                    )}
-                  </div>
-                )}
-                <aside id="syllabusContainer" aria-label={translation.courseLabels.label_syllabus_pdf_header}>
-                  <h3 className="t4">{translation.courseLabels.label_syllabus_pdf_header}</h3>
-                  {courseData.syllabusList[activeSyllabusIndex] &&
-                  courseData.syllabusList[activeSyllabusIndex].course_valid_from &&
-                  courseData.syllabusList[activeSyllabusIndex].course_valid_from[0] ? (
-                    <>
-                      <p>{translation.courseLabels.label_syllabus_pdf_info}</p>
-                      <a
-                        href={`${SYLLABUS_URL}${courseCode}-${courseData.syllabusList[
-                          activeSyllabusIndex
-                        ].course_valid_from.join('')}.pdf?lang=${language}`}
-                        id={courseData.syllabusList[activeSyllabusIndex].course_valid_from.join('') + '_active'}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="pdf-link pdf-link-fix pdf-link-last-line"
-                      >
-                        {`${translation.courseLabels.label_syllabus_link} ${courseCode}${` (${
-                          translation.courseInformation.course_short_semester[
-                            courseData.syllabusList[activeSyllabusIndex].course_valid_from[1]
-                          ]
-                        }${courseData.syllabusList[activeSyllabusIndex].course_valid_from[0]}–${
-                          courseData.syllabusList[activeSyllabusIndex].course_valid_to.length > 0
-                            ? translation.courseInformation.course_short_semester[
-                                courseData.syllabusList[activeSyllabusIndex].course_valid_to[1]
-                              ] +
-                              '' +
-                              courseData.syllabusList[activeSyllabusIndex].course_valid_to[0]
-                            : ''
-                        })`}`}
-                      </a>
-                    </>
-                  ) : (
-                    <>{translation.courseLabels.label_syllabus_missing}</>
-                  )}
-                </aside>
-              </Col>
+            <h2>Where does it come from?</h2>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
+              Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
+              Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a
+              Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the
+              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+              (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of
+              ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+              amet..", comes from a line in section 1.10.32.
+            </p>
 
-              {/** ************************************************************************************************************ */}
-              {/*                           LEFT COLUMN - SYLLABUS + OTHER COURSE INFORMATION                                 */}
-              {/** ************************************************************************************************************ */}
-              <Col id="coreContent" md="8" xs="12" className="float-md-left paragraphs">
-                <div key="fade-2" className={` fade-container ${syllabusInfoFade === true ? ' fadeOutIn' : ''} `}>
-                  <Row id="activeSyllabusContainer" key="activeSyllabusContainer">
-                    <Col sm="12">
-                      {courseData.syllabusSemesterList.length === 0 && (
-                        <Alert color="info" aria-live="polite">
-                          <h4>{translation.courseLabels.header_no_syllabus}</h4>
-                          {translation.courseLabels.label_no_syllabus}
-                        </Alert>
-                      )}
-                    </Col>
-                  </Row>
-
-                  {/* --- COURSE INFORMATION CONTAINER---  */}
-                  <CourseSectionList
-                    courseInfo={courseInfo}
-                    syllabusList={courseData.syllabusList[activeSyllabusIndex]}
-                    syllabusSemesterList={courseData.syllabusSemesterList}
-                    showCourseLink={context.showCourseWebbLink}
-                    partToShow="courseContentBlock"
-                    syllabusName={
-                      courseData.syllabusSemesterList.length > 0
-                        ? `${courseCode}${` (${
-                            translation.courseInformation.course_short_semester[
-                              courseData.syllabusList[activeSyllabusIndex].course_valid_from[1]
-                            ]
-                          }${
-                            courseData.syllabusList[activeSyllabusIndex] &&
-                            courseData.syllabusList[activeSyllabusIndex].course_valid_from
-                              ? courseData.syllabusList[activeSyllabusIndex].course_valid_from[0]
-                              : ''
-                          }–${
-                            courseData.syllabusList[activeSyllabusIndex].course_valid_to.length > 0
-                              ? translation.courseInformation.course_short_semester[
-                                  courseData.syllabusList[activeSyllabusIndex].course_valid_to[1]
-                                ] +
-                                '' +
-                                courseData.syllabusList[activeSyllabusIndex].course_valid_to[0]
-                              : ''
-                          })`}`
-                        : ''
-                    }
-                  />
-
-                  {/* ---IF RESEARCH LEVEL: SHOW "Postgraduate course" LINK--  */}
-                  {courseInfo.course_level_code === 'RESEARCH' && (
-                    <span>
-                      <h3>{translation.courseLabels.header_postgraduate_course}</h3>
-                      {translation.courseLabels.label_postgraduate_course}
-                      <a href={`${FORSKARUTB_URL}${courseInfo.course_department_code}`}>
-                        {courseInfo.course_department}
-                      </a>
-                    </span>
-                  )}
-                </div>
-              </Col>
-            </Col>
-          </Row>
+            <p>
+              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections
+              1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact
+              original form, accompanied by English versions from the 1914 translation by H. Rackham.
+            </p>
+          </div>
         </main>
       </Row>
     </div>
