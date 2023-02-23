c95daab9d51fe914f61cbe8ed77c45dafb947dc0
Rao-Sabahat-Ali <raosabahat01@live.com>
2023-02-23 15:27:46 +0100

    feat: Add application code in request body

--- public/js/app/client-context/addClientFunctionsToWebContext.jsx
+++ public/js/app/client-context/addClientFunctionsToWebContext.jsx
@@ -4,11 +4,12 @@ import axios from 'axios'
 
 function getCourseEmployees() {
   const ladokRound = this.courseData.roundList[this.activeSemester][this.activeRoundIndex]
-  const { roundId: ladokRoundId } = ladokRound
+  const { roundId: ladokRoundId, round_application_code: applicationCode } = ladokRound
   const data = {
     courseCode: this.courseCode,
     semester: this.activeSemester,
     ladokRoundIds: [ladokRoundId],
+    applicationCodes: [applicationCode],
   }
   return axios.post(this.paths.ug.rest.api.uri, data).then(response => {
     const { examiners, responsibles, teachers } = response.data
