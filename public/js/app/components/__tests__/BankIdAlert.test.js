6187a43b74035c3f8ffc2c7f3a0f70cacbad2186
victorsd <victorsd@kth.se>
2024-01-29 14:21:04 +0100

    test(KUI-1136): implemented testing for BankIdAlert

--- public/js/app/components/__tests__/BankIdAlert.test.js
+++ public/js/app/components/__tests__/BankIdAlert.test.js
@@ -86,19 +86,69 @@ describe('Comoponent <BankIdAlert>', () => {
     expect(alert).not.toBeInTheDocument()
   })
 
-  test.todo('does not render BankIdAlert without roundSpecified')
-  test.todo('does not render BankIdAlert with non LL fundingType')
-  test.todo('does not render BankIdAlert without contextLang')
+  test('does not render BankIdAlert without roundSpecified', () => {
+    const translation = i18n.messages[TRANSLATION_SWEDISH_INDEX]
+
+    const propsWithoutRoundSelected = {
+      tutoringForm: 'NML',
+      fundingType: 'LL',
+      contextLang: 'sv',
+      roundSpecified: false,
+      translation: translation,
+    }
+    render(
+      <WebContextProvider configIn={context}>
+        <BankIdAlert {...propsWithoutRoundSelected} />
+      </WebContextProvider>
+    )
+
+    const alert = screen.queryByText(
+      `Du behöver ett KTH-konto för att läsa en fristående kurs, som aktiveras med Mobilt BankID eller genom att besöka KTH:s campus. Det enda sättet att starta en kurs utan att besöka campus, är om du har Mobilt BankID.`
+    )
+    expect(alert).not.toBeInTheDocument()
+  })
+
+  test('does not render BankIdAlert with non LL fundingType', () => {
+    const translation = i18n.messages[TRANSLATION_SWEDISH_INDEX]
+
+    const propsWithWrongFundingType = {
+      tutoringForm: 'DST',
+      fundingType: 'VV',
+      contextLang: 'sv',
+      roundSpecified: true,
+      translation: translation,
+    }
+    render(
+      <WebContextProvider configIn={context}>
+        <BankIdAlert {...propsWithWrongFundingType} />
+      </WebContextProvider>
+    )
+
+    const alert = screen.queryByText(
+      `Du behöver ett KTH-konto för att läsa en fristående kurs, som aktiveras med Mobilt BankID eller genom att besöka KTH:s campus. Det enda sättet att starta en kurs utan att besöka campus, är om du har Mobilt BankID.`
+    )
+    expect(alert).not.toBeInTheDocument()
+  })
+  test('does not render BankIdAlert without contextLang', () => {
+    const translation = i18n.messages[TRANSLATION_SWEDISH_INDEX]
+
+    const propsWithoutUserLang = {
+      tutoringForm: 'DST',
+      fundingType: 'LL',
+      contextLang: undefined,
+      roundSpecified: true,
+      translation: translation,
+    }
+
+    render(
+      <WebContextProvider configIn={context}>
+        <BankIdAlert {...propsWithoutUserLang} />
+      </WebContextProvider>
+    )
+
+    const alert = screen.queryByText(
+      `Du behöver ett KTH-konto för att läsa en fristående kurs, som aktiveras med Mobilt BankID eller genom att besöka KTH:s campus. Det enda sättet att starta en kurs utan att besöka campus, är om du har Mobilt BankID.`
+    )
+    expect(alert).not.toBeInTheDocument()
+  })
 })
-
-// from context:
-// courseData
-// activeSemesters
-// showRoundData
-// useStartSemesterFromQuery
-
-// hasToShowRoundsData
-// const hasToShowRoundsData = showRoundData || (useStartSemesterFromQuery && hasOnlyOneRound)
-// const hasOnlyOneRound = activeSemester?.length > 0 && courseData.roundList[activeSemester].length === 1
-
-// translation
