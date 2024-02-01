57a91621b1219aba20a71a07d30ef322a41e7d36
Karl Andin <karl.andin@drakryggen.com>
2024-02-01 08:10:11 +0100

    add course department utils. change course_department_link for SU

--- server/util/constants.js
+++ server/util/constants.js
@@ -1,19 +1,21 @@
 const PROGRAMME_URL = '/student/kurser/program'
 const INFORM_IF_IMPORTANT_INFO_IS_MISSING = {
   en: '<i>No information inserted</i>',
-  sv: '<i>Ingen information tillagd</i>'
+  sv: '<i>Ingen information tillagd</i>',
 }
 const INFORM_IF_IMPORTANT_INFO_IS_MISSING_ABOUT_MIN_FIELD_OF_STUDY = {
   en: 'This course does not belong to any Main field of study.',
-  sv: 'Denna kurs tillhör inget huvudområde.'
+  sv: 'Denna kurs tillhör inget huvudområde.',
 }
 const MAX_1_MONTH = 3
 const MAX_2_MONTH = 9
+const DEPARTMENT_CODE_STOCKHOLM_UNIVERSITY = 'UL'
 
 module.exports = {
   PROGRAMME_URL,
   INFORM_IF_IMPORTANT_INFO_IS_MISSING,
   INFORM_IF_IMPORTANT_INFO_IS_MISSING_ABOUT_MIN_FIELD_OF_STUDY,
   MAX_1_MONTH,
-  MAX_2_MONTH
+  MAX_2_MONTH,
+  DEPARTMENT_CODE_STOCKHOLM_UNIVERSITY,
 }
