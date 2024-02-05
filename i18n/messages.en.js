70292e5d92e0aed3f370f0f0e46f90c42ab5c0d6
victorsd <victorsd@kth.se>
2024-02-05 10:26:20 +0100

    fix(KUI-1136): now linking to the correct page

--- i18n/messages.en.js
+++ i18n/messages.en.js
@@ -34,7 +34,7 @@ module.exports = {
     skip_to_main_content: 'Skip to main content',
     back_to_top_label: 'To page top',
   },
-  bankIdAlertText: `Please note that <a href="https://intra.kth.se/en/utbildning/tentamen-och-schema/lasarsindelning/lasarsindelning-1.1201135" class="external-link" target="_blank" rel="noopener noreferrer">Students not located in Sweden may have problems attending a course at KTH.</a> <br/> You could meet obstacles if you're required to pay fees or if you do not have a Swedish Mobile BankID. `,
+  bankIdAlertText: `Please note that <a href="https://www.kth.se/en/studies/freestanding-courses/applying-to-a-course-if-you-are-not-in-sweden-1.1275545" class="external-link" target="_blank" rel="noopener noreferrer">Students not located in Sweden may have problems attending a course at KTH.</a> <br/> You could meet obstacles if you're required to pay fees or if you do not have a Swedish Mobile BankID. `,
   breadCrumbLabels: {
     breadcrumbs: 'Breadcrumbs',
     university: 'KTH',
