5aad681ccf153856825183c84a1c4c11bbb92cea
Benjamin Langlotz <benjala@kth.se>
2024-03-27 09:41:29 +0100

    doc(KUI-1257): add comment to i18n

--- i18n/index.js
+++ i18n/index.js
@@ -2,6 +2,7 @@
 /**
  * Extension of the i18n module that depends on the server configuration
  * and is therefore not consumable by the server.
+ * Also, the getCookie() method by definition only works client-side.
  */
 
 const i18n = require('kth-node-i18n')
