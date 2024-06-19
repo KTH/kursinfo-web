897550abc1a1f64e33b3e92b271d814273b76350
Karl Andin <karl.andin@drakryggen.com>
2024-06-19 20:38:57 +0200

    feat(): add headings to info grid

--- public/js/app/components/RoundInformation/RoundInformationInfoGrid.jsx
+++ public/js/app/components/RoundInformation/RoundInformationInfoGrid.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { useLanguage } from '../../hooks/useLanguage'
+import { useMissingInfo } from '../../hooks/useMissingInfo'
 
-function RoundInformationInfoGrid() {
-  return <div className="roundInformation__infoGrid">TODO: Info grid</div>
+const Item = ({ children, title }) => (
+  <div>
+    <h4>{title}</h4>
+    <p>TODO</p>
+    {/* <p>{children}</p> */}
+  </div>
+)
+
+function RoundInformationInfoGrid({ courseRound }) {
+  const { translation } = useLanguage()
+  const { missingInfoLabel } = useMissingInfo()
+  return (
+    <div className="roundInformation__infoGrid">
+      <Item title={translation.courseRoundInformation.round_course_place}></Item>
+      <Item title={translation.courseRoundInformation.round_start_date}></Item>
+      <Item title={translation.courseRoundInformation.round_periods}></Item>
+      <Item title={translation.courseRoundInformation.round_pace_of_study}></Item>
+      <Item title={translation.courseRoundInformation.round_application_code}></Item>
+      <Item title={translation.courseRoundInformation.round_tutoring_form}></Item>
+      <Item title={translation.courseRoundInformation.round_tutoring_language}></Item>
+      <Item title={translation.courseLabels.label_course_memo}></Item>
+      <Item title={translation.courseRoundInformation.round_max_seats}></Item>
+      <Item title={translation.courseRoundInformation.round_part_of_programme}></Item>
+      <Item title={translation.courseRoundInformation.round_target_group}></Item>
+      <Item title={translation.courseRoundInformation.round_time_slots}></Item>
+      <Item title={translation.courseLabels.label_schedule}></Item>
+    </div>
+  )
 }
 
 export { RoundInformationInfoGrid }
