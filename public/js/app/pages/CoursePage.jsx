02fa2b9448114154113aabc866d71a3d9aab61fb
Axel Björkqvist <axelbjo@kth.se>
2024-07-23 16:05:24 +0200

    Removed comment, renamed input parameter, removed ternary

--- public/js/app/pages/CoursePage.jsx
+++ public/js/app/pages/CoursePage.jsx
@@ -129,7 +129,7 @@ function CoursePage() {
         <CourseTitle
           key="title"
           courseTitleData={courseData.courseTitleData}
-          preparatory={courseData.courseInfo.course_level_code}
+          courseLevelCode={courseData.courseInfo.course_level_code}
           language={languageShortname}
           pageTitle={translation.courseLabels.sideMenu.page_before_course}
         />
