4f499b2838d79c5d0aee0d0000f3735683255140
amirhossein-haerian <ahae@kth.se>
2024-09-03 13:00:10 +0200

    fix(KUI-1394): change the popup text

--- i18n/messages.en.js
+++ i18n/messages.en.js
@@ -104,7 +104,7 @@ module.exports = {
       'Course offering may be cancelled if number of admitted are less than minimum of places. If there are more applicants than number of places selection will be made.',
     round_seats_info: 'The selection results are based on:',
     syllabus_info:
-      '<p>• A course goes different course offerings. To see information about a specific course offering, choose semester and course offering. The course syllabus information will be updated depending on the chosen semester. Information from the course syllabus is marked with *.</p><p>• Please note: regulations in course syllabus are rules that are generally applicable and binding for both employees and students.</p><p>• If you have not chosen semester and course offering, you will see course information from the current or future course syllabus. The valid period of the course syllabus is stated on the page.</p>',
+      '<p>A course goes different course offerings. To see information about a specific course offering, choose semester and course offering. The course syllabus information will be updated depending on the chosen semester. Information from the course syllabus is marked with *</p>',
     sideMenu: {
       aria_label: 'Sub menu',
       page_about_course: 'About course',
