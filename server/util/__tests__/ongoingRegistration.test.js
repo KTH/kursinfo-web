e4cb0214b6b3b8ef629dd1a1c95bf8b45f8c0add
Axel Björkqvist <axelbjo@kth.se>
2025-06-10 16:26:41 +0200

    test(KUI-1520): refactoring and more testing of the logic

--- server/util/__tests__/ongoingRegistration.test.js
+++ server/util/__tests__/ongoingRegistration.test.js
@@ -1,4 +1,4 @@
-import { checkIfOngoingRegistration } from '../ongoingRegistration'
+import { checkIfOngoingRegistration, findMatchedPeriod, computeFirstRegistrationDate } from '../ongoingRegistration'
 
 const mockedPeriods = [
   {
@@ -11,6 +11,18 @@ const mockedPeriods = [
     PeriodtypID: '2',
     Slutdatum: '2026-01-12',
     Startdatum: '2025-08-25',
+    link: [],
+  },
+  {
+    Benamning: { sv: 'Kalenderår 2025', en: 'Calendar year 2025' },
+    Beskrivning: {},
+    Dolt: false,
+    Giltighetsperiod: { Slutdatum: '2025-12-31', Startdatum: '2025-01-01', link: [] },
+    ID: '29156',
+    Kod: '2025',
+    PeriodtypID: '1',
+    Slutdatum: '2025-12-31',
+    Startdatum: '2025-01-01',
     link: [],
   },
 ]
@@ -29,12 +41,20 @@ describe('Tests the logic of the checkIfOngoingRegistration function', () => {
     jest.setSystemTime(new Date('2025-10-01'))
     expect(checkIfOngoingRegistration('2025-10-02', mockedPeriods)).toEqual(true)
   })
-  it('Should return false if the current date (today) is NOT within the registration period', () => {
+  it('Should return false if the current date is past the registration period', () => {
     jest.setSystemTime(new Date('2025-10-03'))
     expect(checkIfOngoingRegistration('2025-10-02', mockedPeriods)).toEqual(false)
   })
-  it('Should be able to handle dates taking place during the summer', () => {
+  it('Should return false if the current date is before the registration period', () => {
+    jest.setSystemTime(new Date('2025-03-14'))
+    expect(checkIfOngoingRegistration('2025-10-02', mockedPeriods)).toEqual(false)
+  })
+  it('Should return undefined for a date during the summer', () => {
     jest.setSystemTime(new Date('2025-07-03'))
-    expect(checkIfOngoingRegistration('2025-07-05', mockedPeriods)).toEqual(true)
+    expect(findMatchedPeriod('2025-07-05', mockedPeriods)).toEqual(undefined)
+  })
+  it('Should adjust to monday for a first registration date that occurs during a weekend', () => {
+    jest.setSystemTime(new Date('2025-03-15'))
+    expect(computeFirstRegistrationDate('HT', '2025')).toEqual('2025-03-17')
   })
 })
--- server/util/ongoingRegistration.js
+++ server/util/ongoingRegistration.js
@@ -1,36 +1,40 @@
-export const checkIfOngoingRegistration = (startDate, periods) => {
+export const computeFirstRegistrationDate = (semester, year) => {
   const registrationDates = { HT: '03-15', VT: '09-15', ST: '02-15' }
-  let code = ''
-  let semester = ''
-  let year = ''
-  let registrationDateString = ''
+  const registrationDateString = `${year}-${registrationDates[semester]}`
+
+  // Check if registration date occurs on a weekend and adjust to monday in that case
+  const date = new Date(registrationDateString)
+  const day = date.getDay()
+  if (day === 6) date.setDate(date.getDate() + 2)
+  else if (day === 0) date.setDate(date.getDate() + 1)
+  return date.toISOString().split('T')[0]
+}
+
+export const findMatchedPeriod = (startDate, periods) => {
   const matchedPeriod = periods.find(
     period =>
       startDate >= period.Giltighetsperiod.Startdatum &&
       startDate <= period.Giltighetsperiod.Slutdatum &&
       ['HT', 'VT'].includes(period.Kod.slice(0, 2))
   )
+  return matchedPeriod
+}
+
+export const checkIfOngoingRegistration = (startDate, periods) => {
+  let semester = ''
+  let year = ''
+
+  const matchedPeriod = findMatchedPeriod(startDate, periods)
+
   if (!matchedPeriod) {
     // Summer
     semester = 'ST'
     year = startDate.substring(0, 4)
-    registrationDateString = `${year}-${registrationDates.ST}`
   } else {
-    code = matchedPeriod?.Kod
-    semester = code?.slice(0, 2)
-    year = code?.slice(2, 6)
-    registrationDateString = `${year}-${registrationDates[semester]}`
+    semester = matchedPeriod?.Kod.slice(0, 2)
+    year = matchedPeriod?.Kod.slice(2, 6)
   }
-
-  const date = new Date(registrationDateString)
-  const day = date.getDay()
-
-  if (day === 6) {
-    date.setDate(date.getDate() + 2)
-  } else if (day === 0) {
-    date.setDate(date.getDate() + 1)
-  }
-  const [firstRegistrationDate] = date.toISOString().split('T')
+  const firstRegistrationDate = computeFirstRegistrationDate(semester, year)
 
   const currentDate = new Date()
   const [formattedCurrentDate] = currentDate.toISOString().split('T')
