07e1173b7e0beffe1a313be7079f467051aacea1
Benjamin Langlotz <benjala@kth.se>
2025-11-20 15:29:23 +0100

    ref(KUI-2072): remove unused field

--- server/controllers/createSyllabusList.js
+++ server/controllers/createSyllabusList.js
@@ -15,7 +15,6 @@ const _createEmptySyllabusData = language => ({
   course_ethical: '',
   course_additional_regulations: '',
   course_transitional_reg: '',
-  course_decision_to_discontinue: '',
 })
 
 const _mapSyllabus = (syllabus, language) => {
@@ -35,7 +34,6 @@ const _mapSyllabus = (syllabus, language) => {
     course_ethical: parseOrSetEmpty(syllabus.kursplan.etisktforhallningssatt, language, true),
     course_additional_regulations: parseOrSetEmpty(syllabus.kursplan.ovrigaForeskrifter, language, true),
     course_transitional_reg: parseOrSetEmpty(syllabus.course.overgangsbestammelser, language, true),
-    course_decision_to_discontinue: parseOrSetEmpty(syllabus.kursplan.avvecklingsbeslut, language),
   }
 }
 
